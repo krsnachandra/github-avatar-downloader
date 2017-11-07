ef8f8e2290e3892d5a27d3064829bc7178f7d8f8
Chandra <krsna.c@gmail.com>
2017-11-07 00:32:38 +0000

    implemented authorization and printAvatarURLs function

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,33 @@
 const request = require('request');
 
+const secret = require('./secrets.js');
+
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log(secret.GITHUB_TOKEN);
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  cb();
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    let parsedBody = JSON.parse(body);
+    cb(parsedBody);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function printAvatarURLs(contribArr) {
+  contribArr.forEach(function(contributor) {
+    console.log(contributor['avatar_url']);
+  })
+};
+
+getRepoContributors("jquery", "jquery", printAvatarURLs);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
